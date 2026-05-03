ec9707d32c58ea709a31a349c4e6477eb9f379b1
GLiTcH <148196994+dhanush-shetty1@users.noreply.github.com>
2026-05-03 13:09:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (51.56%) - LeetSync

--- 812-rotate-string/rotate-string.py
+++ 812-rotate-string/rotate-string.py
@@ -1,12 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        if len(s)!=len(goal):
-            return False
-
-        for i in range(len(s)):
-            s=s[1:]+s[0]
-
-            if s==goal:
-                return True
-        
-        return False
+        return True if len(s)==len(goal) and goal in (s+s) else False
