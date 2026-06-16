ddc5be27321e5444ffd5a145c080de70588209a4
GLiTcH <148196994+dhanush-shetty1@users.noreply.github.com>
2026-06-16 12:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.3 MB (82.24%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -1,21 +1,16 @@
 class Solution:
     def processStr(self, s: str) -> str:
-        result=""
-        special=["%","*","#"]
-        for i in range(len(s)):
-            if s[i] not in special:
-                result+=s[i]
-            elif s[i]=="#":
-                result+=result
-            elif s[i]=="%":
-                result=result[::-1]
-            else:
-                result=list(result)
-                if len(result)<=0:
-                    pass
-                else:
-                    result.pop(-1)
-                result="".join(result)
-        return result
+        result = ""
 
-        
+        for ch in s:
+            if ch == "#":
+                result += result
+            elif ch == "%":
+                result = result[::-1]
+            elif ch == "*":
+                if result:
+                    result = result[:-1]
+            else:
+                result += ch
+
+        return result
